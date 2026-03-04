47d0e07c9ad813c0063c4306f65dfbfe354f2379
Nick Hamze <me@nickhamze.com>
2026-03-04 13:45:06 -0600

    Final Fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1046ef04bfcb74ad18df');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c8858fc2fd913264d449');
